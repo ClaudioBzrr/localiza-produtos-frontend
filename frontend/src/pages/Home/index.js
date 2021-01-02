9b547633684ad49cef7feb63533f2743845feb88
= <=>
2021-01-02 02:24:35 -0300

    concluindo a funcionalidade de busca

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,22 +1,115 @@
 import React from 'react'
+import {useState} from 'react'
 import './../../assets/styles/global.css'
 import './styles.css'
 import Header from './../../components/Header'
-import {FiSearch as Search, FiTrash2 as Trash, FiEdit as Edit} from 'react-icons/fi'
+import {FiSearch as Search, FiTrash2 as Trash, FiEdit as Edit, FiSave as Save} from 'react-icons/fi'
+import api from '../../services/api'
+import swal from 'sweetalert'
 
 
 export default function Home() {
+
+    // declaração de contantes
+    const  [arg,setArg] =  useState('')
+    const  [desc_product,setDesc_product] =  useState('')
+    const  [column_product,setColumn_product] =  useState('')
+    const [products,setProducts] =  useState([])
+    const [editable,setEditable] =  useState(false)
+
+    
+    //carregando os produtos
+    // useEffect(()=>{
+    //     api.get('products').then(response => setProducts(response.data))
+
+    // })
+
+
+    // função para buscar produtos
+    async function handleSearchProducts(e){
+        e.preventDefault()
+
+        if(arg ===''){
+            try{
+                await api.get('/products').then(response => setProducts(response.data))
+            }catch(err){
+                swal({
+                    icon:'error',
+                    title:'Erro ao procurar produto',
+                    text:`${err}`
+                })
+            }
+        }else{
+            try{
+                await api.get(`products/search/${arg}`).then(response => response.data == [''] ? swal({icon:'error',title:'Produto não encontrado'}):setProducts(response.data))
+            }catch(err){
+                swal({
+                    icon:'error',
+                    title:'Erro ao buscar produto',
+                    text:`${err}`
+                })
+            }
+        }
+    }
+
+    //função para deletar os produtos
+    function handleDeleteProducts(sku_product){
+        swal({icon:'info',title:'Deseja deletar o produto selecionado ?',buttons:true,dangerMode:true})
+        .then(async (willDelete) =>{
+            if(willDelete){
+                await api.delete(`products/${sku_product}`)
+                setProducts(products.filter(product => product.sku_product !== sku_product))
+                swal({icon:'success',title:'Produto deletado com sucesso'})
+
+            }else{
+                swal({icon:'info',title:'O produto não foi deletado'})
+            }
+        })
+    
+    }
+
+
+    //função pra salvar produtos(através do contentEditable) sem ter que ir para outra página.
+    async function HandleUpdateProducts(sku){
+        const data = {desc_product,column_product}
+
+            try{
+    
+                await api.put(`products/${sku}`,data)
+                swal({
+                    icon:"success",
+                    title:"Alterações salvas com sucesso"
+                })
+            }catch(err){
+                alert(err)
+            }
+            setEditable(!editable)
+        
+    }
+    
+
+
     return (
         <main>
             <Header />
+            <form onSubmit={handleSearchProducts}>
+                
             <div id="localizar">
                 <div id="caixa-pesquisa" >
                     <Search id="icon-search" />
-                    <input id="input-pesquisa" type="search" name="search" placeholder="Pesquisar produtos..." />
+                    <input id="input-pesquisa" type="search" name="search" 
+                    autoComplete='off'
+                    placeholder="Pesquisar produtos..." 
+                    value={arg}
+                    onChange={e =>setArg(e.target.value)}
+                    />
 
                 </div>
-                <button id="botao-buscar">Buscar</button>
+                <button id="botao-buscar" 
+                type='submit'
+                >Buscar</button>
             </div>
+            </form>
 
             <div id="list">
                 <ul id="list-heading">
@@ -29,100 +122,46 @@ export default function Home() {
 
             <div id="data">
                 <ul id="items">
-                    <li id="list-items">
-                        <div id="item-sku">1234</div>
-                        <div id="item-desc">tal tal</div>
-                        <div id="item-col">1</div>
-                        <div id="item-act"><Trash className="act"/><Edit className="act"/></div>
+
+
+
+                    {
+                        products.map(product => 
+      
+                        
+                        
+                        (
+                            
+
+                    <li id="list-items" key={product.sku_product}>
+                        <div id="item-sku" >{product.sku_product}</div>
+
+                        <div id="item-desc"
+                        suppressContentEditableWarning={true}
+                        onBlur={e  => e.target.innerText ===""? setDesc_product(product.desc_product):setDesc_product(e.target.innerText)}
+                        contentEditable={editable}
+                        >{product.desc_product}</div>
+                        
+                        <div id="item-col"
+                        suppressContentEditableWarning={true}
+                        onBlur={e  => desc_product === ""? setDesc_product(product.desc_product):setColumn_product(e.target.innerText)} 
+                        contentEditable={editable}
+                        >{product.column_product}</div>
+
+
+                        <div id="item-act">
+                            <Trash onClick={() => handleDeleteProducts(product.sku_product)} className="act"/>
+                            <Edit onClick={()=>setEditable(!editable)} className="act"/>
+                            <Save onClick={()=>HandleUpdateProducts(product.sku_product)} className="act"/>
+
+                        </div>
                     </li>
 
-                    <li id="list-items">
-                        <div id="item-sku">12345678910</div>
-                        <div id="item-desc">tal taltal taltal taltal taltal tal</div>
-                        <div id="item-col">1</div>
-                        <div id="item-act"><Trash  className="act"/><Edit className="act"/></div>
-                    </li>
-                    <li id="list-items">
-                        <div id="item-sku">12345678910</div>
-                        <div id="item-desc">tal taltal taltal taltal taltal tal</div>
-                        <div id="item-col">1</div>
-                        <div id="item-act"><Trash  className="act"/><Edit className="act"/></div>
-                    </li>
+                        )
 
-                    <li id="list-items">
-                        <div id="item-sku">12345678910</div>
-                        <div id="item-desc">tal taltal taltal taltal taltal tal</div>
-                        <div id="item-col">1</div>
-                        <div id="item-act"><Trash  className="act"/><Edit className="act"/></div>
-                    </li>
+                        )
+                    }
 
-                    <li id="list-items">
-                        <div id="item-sku">12345678910</div>
-                        <div id="item-desc">tal taltal taltal taltal taltal tal</div>
-                        <div id="item-col">1</div>
-                        <div id="item-act"><Trash  className="act"/><Edit className="act"/></div>
-                    </li>
-                    <li id="list-items">
-                        <div id="item-sku">12345678910</div>
-                        <div id="item-desc">tal taltal taltal taltal taltal tal</div>
-                        <div id="item-col">1</div>
-                        <div id="item-act"><Trash  className="act"/><Edit className="act"/></div>
-                    </li>
-                    <li id="list-items">
-                        <div id="item-sku">12345678910</div>
-                        <div id="item-desc">tal taltal taltal taltal taltal tal</div>
-                        <div id="item-col">1</div>
-                        <div id="item-act"><Trash  className="act"/><Edit className="act"/></div>
-                    </li>
-
-                    <li id="list-items">
-                        <div id="item-sku">12345678910</div>
-                        <div id="item-desc">tal taltal taltal taltal taltal tal</div>
-                        <div id="item-col">1</div>
-                        <div id="item-act"><Trash  className="act"/><Edit className="act"/></div>
-                    </li>
-                    <li id="list-items">
-                        <div id="item-sku">12345678910</div>
-                        <div id="item-desc">tal taltal taltal taltal taltal tal</div>
-                        <div id="item-col">1</div>
-                        <div id="item-act"><Trash  className="act"/><Edit className="act"/></div>
-                    </li>
-                    <li id="list-items">
-                        <div id="item-sku">12345678910</div>
-                        <div id="item-desc">tal taltal taltal taltal taltal tal</div>
-                        <div id="item-col">1</div>
-                        <div id="item-act"><Trash  className="act"/><Edit className="act"/></div>
-                    </li>
-                    <li id="list-items">
-                        <div id="item-sku">12345678910</div>
-                        <div id="item-desc">tal taltal taltal taltal taltal tal</div>
-                        <div id="item-col">1</div>
-                        <div id="item-act"><Trash  className="act"/><Edit className="act"/></div>
-                    </li>
-                    <li id="list-items">
-                        <div id="item-sku">12345678910</div>
-                        <div id="item-desc">tal taltal taltal taltal taltal tal</div>
-                        <div id="item-col">1</div>
-                        <div id="item-act"><Trash  className="act"/><Edit className="act"/></div>
-                    </li>
-                    <li id="list-items">
-                        <div id="item-sku">12345678910</div>
-                        <div id="item-desc">tal taltal taltal taltal taltal tal</div>
-                        <div id="item-col">1</div>
-                        <div id="item-act"><Trash  className="act"/><Edit className="act"/></div>
-                    </li>
-                    <li id="list-items">
-                        <div id="item-sku">12345678910</div>
-                        <div id="item-desc">tal taltal taltal taltal taltal tal</div>
-                        <div id="item-col">1</div>
-                        <div id="item-act"><Trash  className="act"/><Edit className="act"/></div>
-                    </li>
-                    <li id="list-items">
-                        <div id="item-sku">12345678910</div>
-                        <div id="item-desc">tal taltal taltal taltal taltal tal</div>
-                        <div id="item-col">1</div>
-                        <div id="item-act"><Trash  className="act"/><Edit className="act"/></div>
-                    </li>
                 </ul>
             </div>
         </main>
--- frontend/src/pages/RegisterProducts/index.js
+++ frontend/src/pages/RegisterProducts/index.js
@@ -1,26 +1,74 @@
 import React from 'react'
+import {useState} from 'react'
+import {useHistory} from 'react-router-dom'
 import './../../assets/styles/global.css'
 import './styles.css'
 import {Link} from 'react-router-dom'
 import {FiArrowLeft as Backlink} from 'react-icons/fi'
+import api from './../../services/api'
+import swal from 'sweetalert'
 
 
 export default function RegisterProducts(){
+
+    const [sku_product,setSku_product] =  useState('')
+    const  [desc_product,setDesc_product] =  useState('')
+    const  [column_product,setColumn_product] =  useState('')
+    const history =  useHistory()
+
+
+
+   
+
+    async function handleRegisterProduct(e){
+        e.preventDefault()
+        const data = {
+            sku_product,
+            desc_product,
+            column_product
+        }
+
+        try{
+              await api.post('products',data)
+            swal("Produto cadastrado com sucesso!!!","","success")
+            history.push('/register')
+
+        }catch(err){
+            swal(`erro : ${err}`,"","error")
+        }
+
+    }
+
+
+
+
     return(
         <main id="main-register">
             <div id="box-register">
                 <Link  id="back-link" to='/'><Backlink/>Voltar</Link>
                 <h1 id="title-register">Cadastro de produtos</h1>
-                <form id="form-register">
+                <form id="form-register"  onSubmit={handleRegisterProduct}>
 
                     <label htmlFor="sku" id="label-sku">Código : </label>
-                    <input name="sku" type="text" id="input-sku"/>
+                    <input name="sku" type="text" id="input-sku"
+                    autoComplete="off"
+                    value={sku_product}
+                    onChange={e => setSku_product(e.target.value)}
+                    />
 
                     <label htmlFor="desc" id="label-desc">Descrição : </label>
-                    <input id="input-desc"name="desc" type="text"/>
+                    <input id="input-desc"name="desc" type="text"
+                    autoComplete="off"
+                    value={desc_product}
+                    onChange={e => setDesc_product(e.target.value)}
+                    />
 
                     <label htmlFor="col" id="label-col">Coluna : </label>
-                    <input id="input-col" name="col" type="text"/>
+                    <input id="input-col" name="col" type="text"
+                    autoComplete="off"
+                    value={column_product}
+                    onChange={e => setColumn_product(e.target.value)}
+                    />
 
                     <button id="button-register"type="submit">Cadastrar</button>
 
