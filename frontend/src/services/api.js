1c1c1d8bbedb487cb9f27475688518da2270c109
ClaudioBzrr <claudio.bezerra1998@gmail.com>
2021-01-15 00:28:19 -0300

    https adicionado a axio baseURL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:'http://localiza-produtos-backend.herokuapp.com/'
+    baseURL:'https://localiza-produtos-backend.herokuapp.com/'
 })
 
 
