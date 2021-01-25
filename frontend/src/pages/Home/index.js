0591229c3c75369d75c0abf06898727702649569
ClaudioBzrr <claudio.bezerra1998@gmail.com>
2021-01-24 23:48:23 -0300

    modal de edição inicado

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,22 +1,53 @@
 import React from 'react'
 import {useState} from 'react'
+import {useHistory} from 'react-router-dom'
 import './../../assets/styles/global.css'
 import './styles.css'
 import Header from './../../components/Header'
-import {FiSearch as Search, FiTrash2 as Trash, FiEye as Eye, FiEdit as Edit} from 'react-icons/fi'
+import {FiSearch as Search, FiTrash2 as Trash, FiEdit as Edit} from 'react-icons/fi'
 import api from '../../services/api'
 import swal from 'sweetalert'
-import {Dialog } from '@material-ui/core'
+import {Dialog, DialogTitle,DialogContent,DialogActions ,Button,TextField, makeStyles} from '@material-ui/core'
 
 
 
 export default function Home() {
 
     // declaração de constantes
+    const history =  useHistory()
+    const [open,setOpen] =  useState(false)
     const [loading,setLoading] = useState(false)
     const  [arg,setArg] =  useState('')
     const [products,setProducts] =  useState([])
+    const [desc_product,setDesc_product] = useState('')
+    const [column_product,setColumn_product] = useState('')
 
+
+
+// configurações do modal de edição de produtos
+
+    const handleOpen = () => {
+
+        setOpen(true);
+      };
+    
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+// função apra cancelar o modal de edição
+
+
+    function handleCloseEdit(e){
+        e.preventDefault()
+        handleClose()
+
+    }
+
+// função para editar os capturar os dados ao editar os produtos
+
+
+    // const stylesModal = makeStyles
 
     
     // função para buscar produtos
@@ -25,7 +56,7 @@ export default function Home() {
         e.preventDefault()
         if(arg ===''){
             try{
-                await api.get('/products').then(response => setProducts(response.data))
+                await api.get('products').then(response => setProducts(response.data))
                 setLoading(false)
 
             }catch(err){
@@ -66,6 +97,22 @@ export default function Home() {
             }
         })
     
+    }
+
+    // Função para editar os produtos
+
+    async function handleEditProducts(e,sku_product){
+        e.preventDefault()
+        const dataEdit ={desc_product, column_product}
+        await api.put(`products/${sku_product}`,dataEdit)
+        handleClose()
+        swal({icon:'success',title:'Produto alterado com sucesso'})
+
+
+
+
+        
+        
     }
 
 
@@ -111,14 +158,16 @@ export default function Home() {
 
                     {
 
-                        loading ? (<div className="sk-chase">
-                        <div className="sk-chase-dot"></div>
-                        <div className="sk-chase-dot"></div>
-                        <div className="sk-chase-dot"></div>
-                        <div className="sk-chase-dot"></div>
-                        <div className="sk-chase-dot"></div>
-                        <div className="sk-chase-dot"></div>
-                      </div>) : 
+                        loading ? (
+                        <div className="sk-chase">
+                            <div className="sk-chase-dot"></div>
+                            <div className="sk-chase-dot"></div>
+                            <div className="sk-chase-dot"></div>
+                            <div className="sk-chase-dot"></div>
+                            <div className="sk-chase-dot"></div>
+                            <div className="sk-chase-dot"></div>
+                      </div>
+                      ) : 
                         products.map(product => 
       
                         
@@ -134,7 +183,34 @@ export default function Home() {
 
 
                         <div id="item-act">
-                            <Edit className='act'/>
+                            <Edit className='act' onClick={handleOpen}/>
+                                    <Dialog open={open} onClose={handleClose} id="modal-edit" >
+
+                                        <DialogTitle>Editar produto</DialogTitle>
+
+                                        <form onSubmit={(e) => handleEditProducts(e,product.sku_product)}>
+
+                                            <DialogContent>
+                                                <TextField label="Código" defaultValue={product.sku_product} inputProps={{ readOnly: true }} disabled={true} fullWidth margin="dense" />
+
+                                                <TextField label="Descrição" defaultValue={product.desc_product} fullWidth margin="dense"
+                                                    onChange={e => e.target.value =='' ? setDesc_product(product.desc_product):setDesc_product(e.target.value)} />
+
+                                                <TextField label="Coluna" defaultValue={product.column_product} fullWidth margin="dense"
+                                                    onChange={e => e.target.value =='' ?setColumn_product(product.column_product):setColumn_product(e.target.value) } />
+                                            </DialogContent>
+
+                                            <DialogActions>
+
+                                                <Button color="secondary" variant="contained" onClick={e => handleCloseEdit(e)} type="text" >
+                                                    Cancelar
+                                                </Button>
+                                                <Button variant="contained" color="primary" type="submit">
+                                                    Salvar
+                                            </Button>
+                                            </DialogActions>
+                                        </form>
+                                    </Dialog>
                             <Trash className='act'
                             onClick={() => handleDeleteProducts(product.sku_product)}
                             />
